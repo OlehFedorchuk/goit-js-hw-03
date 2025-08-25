d78d4cb320fdd1ae0d20e8cb78402eb9cb75c4e7
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2025-08-25 18:11:59 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,12 @@
 // Генератор slug
 
 function slugify(title) {
-  let slug = "";
-  slug = title.toLowerCase();
-  return slug.split(" ").join("-");
+    let slug = title.toLowerCase();
+
+    return slug.split(' ').join('-');
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
